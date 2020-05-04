2141cfb31622d729afae74d22dd29a4f3c9a1596
Mays Majeed <mays.majeed2071@gmail.com>
2020-05-04 19:30:19 +0300

    last updated

--- index.js
+++ index.js
@@ -1,101 +1,103 @@
 const express = require("express");
-const app =express();
+const app = express();
+const validate = require("validate.js");
+const Joi = require("joi");
 
 app.use(express.json());
 
-
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://Mays:maysmlab1@ds363118.mlab.com:63118/atm-db").then(()=>console.log("connected to the database ^_^"))
-.catch(err =>console.error("Connection Faild!"));
-
+mongoose
+  .connect("mongodb://Mays:maysmlab1@ds363118.mlab.com:63118/atm-db")
+  .then(() => console.log("connected to the database ^_^"))
+  .catch((err) => console.error("Connection Faild!"));
 
 const atmSchema = new mongoose.Schema({
-    name: {type: String , required: true , minlength: 2 , maxlength: 10},
-    creationDate: {type: Date ,default: Date.now},
-    haveCash: Boolean,
-    working: Boolean,
-    country: String,
-    city: String,
-    address: String,
-    loc: {
-        type: String,
-        coordinates: [Number]
+  name: String,
+  creationDate: { type: Date, default: Date.now },
+  haveCash: Boolean,
+  working: Boolean,
+  country: String,
+  city: String,
+  address: String,
+  loc: {
+    type: String,
+    coordinates: [Number],
+  },
+});
+
+const AtmCollection2 = mongoose.model("AtmCollection2", atmSchema);
+
+app.get("/api/singleAtm/:id", async (req, res) => {
+  const singleAtm = await AtmCollection2.findById(req.params.id);
+  if (!singleAtm)
+    return res.status(404).send("The ATM with the specific ID is not found!!");
+  res.send(singleAtm);
+});
+
+app.get("/api/atms", async (req, res) => {
+  const allAtms = await AtmCollection2.find();
+  res.send(allAtms);
+});
+
+app.post("/api/Newatm", async (req, res) => {
+  const constraints = {
+    name: {
+      presence: true,
+      length: {
+        minimum: 3,
+        message: "must be at least 6 characters",
+      },
+    },
+  };
+  const check = validate(req.body, constraints);
+  console.log(check);
+
+  if (check.error) {
+    res.status(400).send(res.error);
+    return;
   }
-});
 
-const AtmCollection2 = mongoose.model("AtmCollection2",atmSchema);
-
-/*
-AtmCollection2.create({
-    name: "A1",
-    haveCash: "yes",
-    working: "yes",
-    country: "Iraq",
-    city: "Baghdad",
-    address: "Ziyona",
+  const allAtms = new AtmCollection2({
+    name: req.body.name,
+    haveCash: req.body.haveCash,
+    working: req.body.working,
+    country: req.body.country,
+    city: req.body.city,
+    address: req.body.address,
     loc: {
-        type: "Point",
-        coordinates:  [ 33.3221,//latitude 44.4512 //longitude ]
-    }},function(error,data){
-    if(error){
-        console.log(error);
-    }else{
-        console.log("Successful!");
-    }
-});
-*/
-
-app.get("/api/singleAtm/:id", (req, res) =>{
-    const singleAtm = AtmCollection2.findById(req.params.id);
-    if (!singleAtm) return res.status(404).send("The ATM with the specific ID is not found!!");
-    res.send(singleAtm);
-
+      type: req.body.type,
+      coordinates: [req.body.latitude, req.body.longitude],
+    },
   });
-
-
-app.get("/api/atms",async (req,res)=>{
-    const allAtms = await AtmCollection2.find();
-    res.send(allAtms);
+  allAtms.save();
+  res.send(allAtms);
 });
 
+app.put("/updateAtm/:id", async (req, res) => {
+  const atm = await AtmCollection2.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      haveCash: req.body.haveCash,
+      working: req.body.working,
+      country: req.body.country,
+      city: req.body.city,
+      address: req.body.address,
+    },
+    { new: true }
+  );
 
-app.post("/api/Newatm",async(req,res)=>{
-    const allAtms = new AtmCollection2({name: req.body.name,
-        haveCash: req.body.haveCash,
-        working: req.body.working,
-        country: req.body.country,
-        city: req.body.city,
-        address: req.body.address,
-        loc: {
-            type: req.body.type,
-            coordinates: [req.body.latitude, req.body.longitude]
-        }
-
-    });
-    allAtms.save();
-    res.send(allAtms);
+  if (!atm)
+    return res.status(404).send("The ATM with the specific ID is not found!");
+  res.send(atm);
 });
 
-app.put("/updateAtm/:id",async(req,res)=>{
-    const atm = await AtmCollection2.findByIdAndUpdate(req.params.id,{name: req.body.name,
-        haveCash: req.body.haveCash,
-        working: req.body.working,
-        country: req.body.country,
-        city: req.body.city,
-        address: req.body.address},{new:true});
-    
-    if (!atm) return res.status(404).send('The ATM with the specific ID is not found!');
-    res.send(atm)
-});
-
-app.delete("/deleteAtm/:id",async(req,res)=>{
-    const atm =await AtmCollection2.findByIdAndRemove(req.params.id);
-    if (!atm) return res.status(404).send('The ATM with the specific ID is not found!');
-    res.send(atm)
+app.delete("/deleteAtm/:id", async (req, res) => {
+  const atm = await AtmCollection2.findByIdAndRemove(req.params.id);
+  if (!atm)
+    return res.status(404).send("The ATM with the specific ID is not found!");
+  res.send(atm);
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port,()=>console.log(`listening to ${port}...`));
-  
-
-
+app.listen(port, () => console.log(`listening to ${port}...`));
